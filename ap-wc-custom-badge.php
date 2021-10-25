0aacb04cce108c78e397ced467c35e18479016a7
adrianpajares.com <desarrollo@adrianpajares.com>
2021-10-25 12:33:27 +0200

    Update ap-wc-custom-badge.php

--- ap-wc-custom-badge.php
+++ ap-wc-custom-badge.php
@@ -88,8 +88,10 @@ function ap_new_badge() {
 
     } else {
 
+		$options = get_option( 'ap_tools_settings' );
+
         global $product;
-        $newness_days = 40; // Number of days the "NEW" badge is shown
+        $newness_days = $options['ap_tools_text_field_1']; // Number of days the "NEW" badge is shown
         $created = strtotime( $product->get_date_created() );
 
         // Get product prices
